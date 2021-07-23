ab169fe1d56d8bebd4e68272017e29f1ccc3115a
lysar1e <79013918+lysar1e@users.noreply.github.com>
2021-07-23 22:12:27 +0600

    Delete api directory

--- api/src/models/User.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const schema = new mongoose_1.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-});
-module.exports = mongoose_1.model('User', schema);
